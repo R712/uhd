1864a81f194ed04af018c006937605862912b384
Ahreum Han <R712@users.noreply.github.com>
2019-06-06 08:21:57 +0900

    추가 디자인 페이지
    장바구니+결제 기본 페이지
    아티스트 목록 페이지
    앨범 목록 페이지
    아티스트 단일  페이지
    앨범 단일 페이지

--- js/uhdaudio.js
+++ js/uhdaudio.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+	
+	/* 공통 헤더 로드 (미리보기용) - failed. URL load 방식이 file이라서 jquery에서 보안문제로 막는 듯 ㅠ
+	$( "#header" ).load( "header.html #header" ); */
+	
+	/* 정사각형 썸네일을 위한 - 가로 너비에 맞게 세로 크기 조절하기 */
+	var cardThumbWidth = $('.list-default .card-image').width();
+	$('.list-default .card-image').height(cardThumbWidth);
+	
+	/* 창 높이 가져와서 푸터 위치 맞추기 */
+	var bodyHeight = $('body').height();
+	var headerHeight = $('.header').outerHeight();
+	var footerHeight = $('.footer').outerHeight();
+	var contentHeight = bodyHeight - headerHeight - footerHeight;
+	$('.contents-wrap').css('min-height', contentHeight);
+	
+	/* 브라우저 리사이즈일 때에도 */
+	$(window).resize(function() {
+		/* + 정사각형 썸네일 만들기 */
+		var cardThumbWidth = $('.card-image').width();
+		$('.card-image').height(cardThumbWidth);
+		/* + 창 높이 가져와서 푸터 위치 맞추기 */
+		var bodyHeight = $('body').height();
+		var headerHeight = $('.header').outerHeight();
+		var footerHeight = $('.footer').outerHeight();
+		var contentHeight = bodyHeight - headerHeight - footerHeight;
+		$('.contents-wrap').css('min-height', contentHeight);
+	});
+	
+	/* 미리보기용 장바구니 버튼들 클릭시 장바구니 페이지로 이동 */
+	$('.typcn-shopping-cart').parent('.btn').click(function(){
+	   location.href = 'cart.html'; 
+	});
+	
+	/* 슬라이더 제어용 */
     $('.recommend-artist .owl-carousel').owlCarousel({
         loop: true,
         nav: false,
@@ -28,14 +62,22 @@ $(document).ready(function(){
             1440: { items: 6 }
         }
     });
-    $('.blind, .layer .layer-close').click(function(){
-       $('.layer-set').fadeOut(); 
-    });
+    
+    /* 사이드 GNB 제어용 */
     $('.btn-sidebar').click(function(){
         $('body').toggleClass('sidebar-toggle');
         $(".sidebar").slideToggle();
     });
-    $('.account-set button').click(function(){
+    
+    /* 샘플 팝업 레이어 제어용 */
+    $('.blind, .layer .layer-close').click(function(){
+       $('.layer-set').fadeOut(); 
+    });    $('.account-set button').click(function(){
        $('#welcome-layer').fadeIn('fast');
+    });
+
+    /* 샘플 코드용 페이지 이동 코드. 실제로 필요하지 않음 */
+    $('.img-logo').click(function(){
+	   location.href = 'index.html'; 
     });
 });
